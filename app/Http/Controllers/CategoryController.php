a54b78e220443d6f1103224961c5f06b3dfc63c2
EnzoAmarilla <enzo100amarilla@gmail.com>
2023-07-27 12:15:31 -0300

    endpoint categorias con data de categoria padre

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
 {
     public function get_all_categories()
     {
-        $categories = Category::all();
+        $categories = Category::with('parent_category')->get();
         return response(compact("categories"));
     }
 }
